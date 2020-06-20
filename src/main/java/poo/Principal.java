3ad471a08566d601ba19d0e0dd3863324e71a998
Renan Rodolfo da Silva <renaner123@gmail.com>
2020-06-20 20:31:38 -0300

    Classe de testes

--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -1,14 +1,59 @@
 package poo;
 
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+
 public class Principal {
 
     public static void main(String[] args) {
-        Grafo grafo = new Grafo();
+        Grafo inserir = new Grafo("First Grafo");
 
-          grafo.addEndpoint();
-          grafo.addRoteador();
+        ArrayList<Node> teste = new ArrayList<>();
 
-          System.out.println(grafo.toString());
+        inserir.addEndPoint("PC1",EnumNodeNames.desktop);
+        inserir.addFirewall("Mikrotik");
+        inserir.addFirewall("Mikrotik");
+        inserir.addRoteador("Router Principal");
+        inserir.addInternet("WAN");
+
+        //inserir.mostrarNodes();
+
+    /*
+    O nó endpoint (desktop, câmera IP, servidor) poderá estar associado a no máximo a um outro nó
+    O nó roteador poderá estar associado com 2 ou N outros nós
+    O nó firewall poderá estar associado com 2 ou N outros nós
+    O nó switch (comutador) poderá estar associado com 1 ou N outros nós
+    O nó Internet poderá estar associado com 1 ou N outros nós */
+
+
+        inserir.nodeOrigemToDestino("wan1","router1","firewall2");
+        inserir.nodeOrigemToDestino("firewall1","firewall2","PC1");
+        inserir.nodeOrigemToDestino("firewall2","router1");
+        inserir.nodeOrigemToDestino("desktop1","firewall1","firewall2");
+
+        teste = inserir.getNos();
+
+//        System.out.println(inserir.arcosEntreNosdoGrafo());
+//        System.out.println(inserir.nosDoGrafro());
+
+        for(Node aux : teste){
+            if(aux.getNomeNode().equals("firewall1")){
+                aux.setFontSize(14);
+            }
+        }
+        System.out.println(inserir.toString());
+
+
+
+
+
+
+
+
+
+
+
 
 
 
