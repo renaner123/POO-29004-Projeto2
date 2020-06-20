4fdd7d3788e4741abf3a17144e7be147a98f3122
Renan Rodolfo da Silva <renaner123@gmail.com>
2020-06-20 20:31:10 -0300

    Classe Abstrata que será herdada

--- src/main/java/poo/Node.java
+++ src/main/java/poo/Node.java
@@ -2,37 +2,39 @@ package poo;
 
 import java.util.ArrayList;
 
-public class No{
+public abstract class Node {
 
-    private static final int FONTEPADRAO = 10;
-    private static final String SHAPEPADRAO = "none";
-    private static final String LABELLOCPADRAO = "c";
 
-    private String idNo;
+    private static final int NASSOCIACOES = 10;
+    private static final int RESTRICAO2 = 2;
+    private static final int RESTRICAO1 = 1;
+
+    private String nomeNode;
     private String label;
-    private String labelLoc;  // place the label at the top (b seems to be default)
-    private String shape;     // box, polygon, ellipse, oval, circle, diamond
+    private String labelLoc="";  // place the label at the top (b seems to be default)
+    private String shape="";     // box, polygon, ellipse, oval, circle, diamond
     private String image;     //
     private String fillColor;
-    private String color;
-    private int fontSize;
-    private static int noCreates;
+    private String color="";
+    private int fontSize=0;
+    private ArrayList<String> arcos = new ArrayList<>();
+    private int minAssociacao;
+    private int maxAssociacao;
 
+/*
+    O nó endpoint (desktop, câmera IP, servidor) poderá estar associado a no máximo a um outro nó
+    O nó roteador poderá estar associado com 2 ou N outros nós
+    O nó firewall poderá estar associado com 2 ou N outros nós
+    O nó switch (comutador) poderá estar associado com 1 ou N outros nós
+    O nó Internet poderá estar associado com 1 ou N outros nós
+ */
 
-    public No(String label) {
+    public Node(String label) {
         this.label = label;
-        this.labelLoc = LABELLOCPADRAO;
-        this.fontSize = FONTEPADRAO;
-        this.shape = SHAPEPADRAO;
+        this.nomeNode = label;
     }
-    public No() {
+    public Node() {
     }
-
-    public void criarNo(){};
-    public void modificarNo(){};
-    public void visualisarNo(){};
-
-
 
     public  String getLabel() {
         return label;
@@ -90,11 +92,80 @@ public class No{
         this.fontSize = fontSize;
     }
 
-    public String getIdNo() {
-        return idNo;
+    public String getNomeNode() {
+        return nomeNode;
     }
 
-    public void setIdNo(String idNo) {
-        this.idNo = idNo;
+    public void setNomeNode(String nomeNode) {
+        this.nomeNode = nomeNode;
+    }
+
+
+    public int getMinAssociacao() {
+        return minAssociacao;
+    }
+
+    public void setMinAssociacao(int minAssociacao) {
+        this.minAssociacao = minAssociacao;
+    }
+
+    public int getMaxAssociacao() {
+        return maxAssociacao;
+    }
+
+    public void setMaxAssociacao(int maxAssociacao) {
+        this.maxAssociacao = maxAssociacao;
+    }
+
+    public ArrayList<String> getArcos() {
+        return arcos;
+    }
+
+    public void setArcos(ArrayList<String> arcos) {
+        this.arcos = arcos;
+    }
+
+    public int getNASSOCIACOES() {
+        return NASSOCIACOES;
+    }
+
+    public static int getRESTRICAO2() {
+        return RESTRICAO2;
+    }
+
+    public static int getRESTRICAO1() {
+        return RESTRICAO1;
+    }
+
+    /**
+     *
+     * @return Retorna uma String com todos os valores que não estão vazios ou no valor padrão
+     */
+    public String toStringNotNull(){
+        String auxReturn = "";
+        if(!this.getLabel().equals("null")){
+            auxReturn += "label=\"" + this.getLabel() + "\" ";
+        }if(!this.getLabellLoc().isEmpty()){
+            auxReturn += "labelloc " + this.getLabellLoc() + " ";
+        }if(!this.getShape().isEmpty()){
+            auxReturn += "shape=\"" + this.getShape() + "\" ";
+        }if(!this.getImage().equals("null")){
+            auxReturn += "image=\"" + this.getImage() + "\" ";
+        }if(!this.getColor().isEmpty()){
+            auxReturn += "color=\"" + this.getColor() + "\" ";
+        }if(!this.getShape().isEmpty()){
+            auxReturn += "shape=\"" + this.getShape() + "\" ";
+        }
+        if(this.getFontSize()!=0){
+            auxReturn += "fontsize=" + this.getFontSize() ;
+        }
+        return auxReturn;
+
+    }
+
+
+    @Override
+    public String toString() {
+        return this.getNomeNode() + " [" +toStringNotNull() + "]";
     }
 }
